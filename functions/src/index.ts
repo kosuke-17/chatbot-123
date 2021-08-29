f4cee4de5bb778fa3acbfe87bc8f57ac70b48c3d
kosuke-17 <tamutamu17@icloud.com>
2021-08-29 21:12:47 +0900

    typos

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 admin.initializeApp();
 const db = admin.firestore();
@@ -6,19 +6,19 @@ const db = admin.firestore();
 const sendResponse = (response: functions.Response, statusCode: number, body: any) => {
   response.send({
     statusCode,
-    body: JSON.stringify(body)
-  })
-}
+    body: JSON.stringify(body),
+  });
+};
 
 export const addDataset = functions.https.onRequest(async (res: any, req: any) => {
-  if (req.method !== 'POST') {
-    sendResponse(res,405,{error: 'Invalid Request'})
+  if (req.method !== "POST") {
+    sendResponse(res, 405, {error: "Invalid Request"});
   } else {
-    const dataset = req.body
+    const dataset = req.body;
     for (const key of Object.keys(dataset)) {
-      const data = dataset[key]
-      await db.collection('question').doc(key).set(data)
+      const data = dataset[key];
+      await db.collection("question").doc(key).set(data);
     }
-    sendResponse(res,200,{message: 'Successfully added dataset'})
+    sendResponse(res, 200, {message: "Successfully added dataset"});
   }
-})
+});
