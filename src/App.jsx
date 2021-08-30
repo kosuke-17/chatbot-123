e02593c8efc3b41c878b864328ef2a917b8e6869
kosuke-17 <tamutamu17@icloud.com>
2021-08-31 07:45:38 +0900

    componentDidMountでデータセット

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import defaultDataset from './dataset';
 import './assets/styles/style.css'
 import { AnswersList, Chats } from './components/index';
 import FormDialog from './components/Forms/FormDialog';
@@ -12,7 +11,7 @@ export default class App extends React.Component {
         answers: [],
         chats: [],
         currentId: "init",
-        dataset: defaultDataset,
+        dataset: {},
         open: false
       }
       this.selectAnswer = this.selectAnswer.bind(this)
@@ -81,10 +80,24 @@ export default class App extends React.Component {
     this.setState({ open: false })
   };
 
+  initDataset = (dataset) => {
+    this.setState({dataset: dataset})
+  }
+
   componentDidMount() {
-    db.collection('questions')
-    const initAnswer = ""
-    this.selectAnswer(initAnswer, this.state.currentId)
+    (async() => {
+      const dataset = this.state.dataset
+      await db.collection('questions').get().then((snapshots) => {
+        snapshots.forEach((doc) => {
+          const id = doc.id
+          const data = doc.data()
+          dataset[id] = data
+        })
+      })
+      this.initDataset(dataset)
+      const initAnswer = ""
+      this.selectAnswer(initAnswer, this.state.currentId)
+    })()
   }
   componentDidUpdate() {
     const scrollArea = document.getElementById('scroll-area')
